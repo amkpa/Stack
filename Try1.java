b539817d944a0c79091e4848a16b45d891dab0ed
Amit palai <75921456+amkpa@users.noreply.github.com>
2022-05-14 17:47:07 +0530

    Add files via upload

---
+++ Try1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Try1 {
+	public static void main(String[] args) {
+		List<Float> g = new ArrayList<>();
+		
+		g.add(44.5f);
+		System.out.println(g);
+	}
+
+}
